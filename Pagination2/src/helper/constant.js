ff7aac9daf18d3dd5b6c408668739cc681be54ae
sagarrana <ranasagar974@gmail.com>
2024-08-17 15:01:18 +0530

    tsx module removed

---
+++ Pagination2/src/helper/constant.js
@@ -0,0 +1,419 @@
+const freeCSCourses = [{
+    "name": "Full Stack Open 2024",
+    "description": "Modern web development with JavaScript, React, Node.js, GraphQL, and TypeScript.",
+    "link": "https://fullstackopen.com/en/",
+    "image": "https://fullstackopen.com/static/img/logo.png",
+    "rating": "N/A",
+    "time": "8 weeks",
+    "type": "Web Development"
+},
+{
+    "name": "Introduction to Machine Learning with Python",
+    "description": "Basics of machine learning with Python.",
+    "link": "https://www.mooc.fi/en/",
+    "image": "https://www.helsinki.fi/en/media/introduction-to-machine-learning-with-python.svg",
+    "rating": "N/A",
+    "time": "Approx. 6 weeks",
+    "type": "Machine Learning"
+},
+{
+    "name": "Elementa: Computing and Programming in Python",
+    "description": "Learn basic computing and programming with Python.",
+    "link": "https://www.mooc.fi/en/",
+    "image": "https://www.helsinki.fi/en/media/elementa-computing-and-programming-in-python.svg",
+    "rating": "N/A",
+    "time": "Approx. 6 weeks",
+    "type": "Programming"
+},
+{
+    "name": "AI Programming with Python",
+    "description": "Introduction to AI programming using Python.",
+    "link": "https://www.mooc.fi/en/",
+    "image": "https://www.helsinki.fi/en/media/ai-programming-with-python.svg",
+    "rating": "N/A",
+    "time": "Approx. 6 weeks",
+    "type": "Artificial Intelligence"
+},
+{
+    "name": "Introduction to Data Science",
+    "description": "Learn fundamental data science techniques, including data analysis and visualization.",
+    "link": "https://www.mooc.fi/en/",
+    "image": "https://www.helsinki.fi/en/media/introduction-to-data-science.svg",
+    "rating": "N/A",
+    "time": "Approx. 4 weeks",
+    "type": "Data Science"
+},
+{
+    "name": "Digital Technologies in Education",
+    "description": "Explore how digital technologies can be integrated into educational practices.",
+    "link": "https://www.mooc.fi/en/",
+    "image": "https://www.helsinki.fi/en/media/digital-technologies-in-education.svg",
+    "rating": "N/A",
+    "time": "Approx. 6 weeks",
+    "type": "Education"
+},
+{
+    "name": "Sustainable Development Goals and Social Justice",
+    "description": "Learn about sustainable development goals and their implications for social justice.",
+    "link": "https://www.mooc.fi/en/",
+    "image": "https://www.helsinki.fi/en/media/sustainable-development-goals-and-social-justice.svg",
+    "rating": "N/A",
+    "time": "Approx. 5 weeks",
+    "type": "Social Justice"
+},
+{
+    "name": "Introduction to Cloud Computing",
+    "description": "Basics of cloud computing, including cloud services and deployment models.",
+    "link": "https://www.mooc.fi/en/",
+    "image": "https://www.helsinki.fi/en/media/introduction-to-cloud-computing.svg",
+    "rating": "N/A",
+    "time": "Approx. 4 weeks",
+    "type": "Cloud Computing"
+},
+{
+    "name": "Cyber Security Basics",
+    "description": "Introduction to fundamental concepts of cybersecurity and protection of information systems.",
+    "link": "https://www.mooc.fi/en/",
+    "image": "https://www.helsinki.fi/en/media/cyber-security-basics.svg",
+    "rating": "N/A",
+    "time": "Approx. 4 weeks",
+    "type": "Cyber Security"
+},
+{
+    "name": "Foundations of Quantum Computing",
+    "description": "Explore the fundamental principles of quantum computing.",
+    "link": "https://www.mooc.fi/en/",
+    "image": "https://www.helsinki.fi/en/media/foundations-of-quantum-computing.svg",
+    "rating": "N/A",
+    "time": "Approx. 6 weeks",
+    "type": "Quantum Computing"
+},
+{
+    "name": "Introduction to Data Structures and Algorithms",
+    "description": "Learn the fundamental data structures and algorithms in this comprehensive course.",
+    "link": "https://www.coursera.org/learn/data-structures",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Data Structures & Algorithms",
+    "rating": 4.8,
+    "time": "16 hours"
+},
+{
+    "name": "The Complete Web Developer Course",
+    "description": "Master web development with this extensive course covering HTML, CSS, JavaScript, and more.",
+    "link": "https://www.udemy.com/course/the-complete-web-developer-course/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Web Development",
+    "rating": 4.7,
+    "time": "52 hours"
+},
+{
+    "name": "Machine Learning by Stanford University",
+    "description": "An introductory course on machine learning, covering various algorithms and techniques.",
+    "link": "https://www.coursera.org/learn/machine-learning",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Machine Learning",
+    "rating": 4.9,
+    "time": "11 weeks"
+},
+{
+    "name": "AI For Everyone",
+    "description": "An introduction to artificial intelligence and its applications in various fields.",
+    "link": "https://www.coursera.org/learn/ai-for-everyone",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Artificial Intelligence",
+    "rating": 4.7,
+    "time": "6 hours"
+},
+{
+    "name": "Intro to Computer Science and Programming Using Python",
+    "description": "A comprehensive introduction to computer science and programming with Python.",
+    "link": "https://www.edx.org/course/introduction-to-computer-science-and-programming-using-python",
+    "image": "https://www.edx.org/sites/default/files/favicon-16x16.png",
+    "type": "Programming",
+    "rating": 4.8,
+    "time": "9 weeks"
+},
+{
+    "name": "JavaScript Essentials",
+    "description": "Learn the essentials of JavaScript programming in this beginner-friendly course.",
+    "link": "https://www.udacity.com/course/javascript-basics--ud804",
+    "image": "https://www.udacity.com/favicon.ico",
+    "type": "Web Development",
+    "rating": 4.6,
+    "time": "12 hours"
+},
+{
+    "name": "Deep Learning Specialization",
+    "description": "A series of courses on deep learning, covering neural networks, deep learning models, and more.",
+    "link": "https://www.coursera.org/specializations/deep-learning",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Deep Learning",
+    "rating": 4.8,
+    "time": "3 months"
+},
+{
+    "name": "Introduction to Web Design",
+    "description": "An introductory course to web design, covering design principles and tools.",
+    "link": "https://www.udemy.com/course/web-design-for-beginners/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Web Design",
+    "rating": 4.5,
+    "time": "30 hours"
+},
+{
+    "name": "Introduction to Data Science",
+    "description": "An introductory course on data science, including data analysis, visualization, and more.",
+    "link": "https://www.coursera.org/learn/what-is-datascience",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Data Science",
+    "rating": 4.7,
+    "time": "19 hours"
+},
+{
+    "name": "The Complete Machine Learning Course with Python",
+    "description": "A comprehensive course on machine learning with Python, covering various algorithms and techniques.",
+    "link": "https://www.udemy.com/course/machine-learning-with-python/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Machine Learning",
+    "rating": 4.8,
+    "time": "28 hours"
+},
+{
+    "name": "Introduction to Artificial Intelligence",
+    "description": "Learn the fundamentals of artificial intelligence, including key concepts and applications.",
+    "link": "https://www.edx.org/course/introduction-to-artificial-intelligence",
+    "image": "https://www.edx.org/sites/default/files/favicon-16x16.png",
+    "type": "Artificial Intelligence",
+    "rating": 4.6,
+    "time": "6 weeks"
+},
+{
+    "name": "Data Structures and Algorithms in Python",
+    "description": "A comprehensive course on data structures and algorithms using Python.",
+    "link": "https://www.udemy.com/course/data-structures-and-algorithms-using-python/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Data Structures & Algorithms",
+    "rating": 4.7,
+    "time": "40 hours"
+},
+{
+    "name": "Web Development Bootcamp",
+    "description": "Learn web development from scratch with this comprehensive bootcamp course.",
+    "link": "https://www.udemy.com/course/the-web-developer-bootcamp/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Web Development",
+    "rating": 4.8,
+    "time": "63 hours"
+},
+{
+    "name": "Machine Learning Crash Course",
+    "description": "A quick introduction to machine learning with practical exercises.",
+    "link": "https://www.google.com/intl/en_in/learn/machine-learning/crash-course/",
+    "image": "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Google_Research_Logo.max-150x150.png",
+    "type": "Machine Learning",
+    "rating": 4.8,
+    "time": "15 hours"
+},
+{
+    "name": "Artificial Intelligence: A Modern Approach",
+    "description": "Learn the modern approach to artificial intelligence with this in-depth course.",
+    "link": "https://www.udemy.com/course/artificial-intelligence-a-modern-approach/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Artificial Intelligence",
+    "rating": 4.7,
+    "time": "20 hours"
+},
+{
+    "name": "Introduction to Web Programming",
+    "description": "Learn the basics of web programming including HTML, CSS, and JavaScript.",
+    "link": "https://www.coursera.org/learn/web-programming",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Web Development",
+    "rating": 4.6,
+    "time": "30 hours"
+},
+{
+    "name": "Python for Data Science and Machine Learning Bootcamp",
+    "description": "A comprehensive course on Python for data science and machine learning.",
+    "link": "https://www.udemy.com/course/python-for-data-science-and-machine-learning-bootcamp/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Data Science & Machine Learning",
+    "rating": 4.8,
+    "time": "32 hours"
+},
+{
+    "name": "Introduction to Cybersecurity",
+    "description": "Learn the basics of cybersecurity including protection against threats.",
+    "link": "https://www.coursera.org/learn/intro-cyber-security",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Cybersecurity",
+    "rating": 4.7,
+    "time": "20 hours"
+},
+{
+    "name": "Introduction to Cloud Computing",
+    "description": "Learn the basics of cloud computing and its applications.",
+    "link": "https://www.edx.org/course/introduction-to-cloud-computing",
+    "image": "https://www.edx.org/sites/default/files/favicon-16x16.png",
+    "type": "Cloud Computing",
+    "rating": 4.6,
+    "time": "6 weeks"
+},
+{
+    "name": "Data Analysis with Python",
+    "description": "Learn data analysis techniques using Python's data science libraries.",
+    "link": "https://www.coursera.org/learn/data-analysis-with-python",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Data Analysis",
+    "rating": 4.8,
+    "time": "18 hours"
+},
+{
+    "name": "Building Web Applications with Flask",
+    "description": "Learn how to build web applications using the Flask framework.",
+    "link": "https://www.udemy.com/course/building-web-applications-with-flask/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Web Development",
+    "rating": 4.6,
+    "time": "24 hours"
+},
+{
+    "name": "Deep Learning with TensorFlow",
+    "description": "Learn deep learning techniques using TensorFlow, a popular deep learning library.",
+    "link": "https://www.coursera.org/learn/deep-learning-tensorflow",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Deep Learning",
+    "rating": 4.8,
+    "time": "8 weeks"
+},
+{
+    "name": "Intro to AI Programming with Python",
+    "description": "Learn the basics of artificial intelligence programming with Python.",
+    "link": "https://www.udacity.com/course/intro-to-ai-programming-nanodegree--nd089",
+    "image": "https://www.udacity.com/favicon.ico",
+    "type": "Artificial Intelligence",
+    "rating": 4.7,
+    "time": "3 months"
+},
+{
+    "name": "Algorithms Specialization",
+    "description": "An in-depth specialization covering various algorithms and their applications.",
+    "link": "https://www.coursera.org/specializations/algorithms",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Algorithms",
+    "rating": 4.8,
+    "time": "4 months"
+},
+{
+    "name": "Web Development with JavaScript",
+    "description": "A comprehensive course on JavaScript for web development.",
+    "link": "https://www.udemy.com/course/web-development-with-javascript/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Web Development",
+    "rating": 4.6,
+    "time": "40 hours"
+},
+{
+    "name": "Machine Learning for Everyone",
+    "description": "An introduction to machine learning concepts and techniques for non-experts.",
+    "link": "https://www.coursera.org/learn/machine-learning-for-everyone",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Machine Learning",
+    "rating": 4.7,
+    "time": "12 hours"
+},
+{
+    "name": "Artificial Intelligence in Practice",
+    "description": "Apply artificial intelligence techniques to real-world problems.",
+    "link": "https://www.edx.org/course/artificial-intelligence-in-practice",
+    "image": "https://www.edx.org/sites/default/files/favicon-16x16.png",
+    "type": "Artificial Intelligence",
+    "rating": 4.6,
+    "time": "8 weeks"
+},
+{
+    "name": "Responsive Web Design Fundamentals",
+    "description": "Learn how to design responsive web pages that work across different devices.",
+    "link": "https://www.freecodecamp.org/learn/responsive-web-design/",
+    "image": "https://www.freecodecamp.org/favicon.ico",
+    "type": "Web Design",
+    "rating": 4.7,
+    "time": "12 hours"
+},
+{
+    "name": "Introduction to Machine Learning with Python",
+    "description": "A beginner-friendly course on machine learning with Python.",
+    "link": "https://www.udacity.com/course/introduction-to-machine-learning-with-python--ud120",
+    "image": "https://www.udacity.com/favicon.ico",
+    "type": "Machine Learning",
+    "rating": 4.8,
+    "time": "3 months"
+},
+{
+    "name": "AI for Everyone",
+    "description": "An introduction to artificial intelligence concepts and applications.",
+    "link": "https://www.coursera.org/learn/ai-for-everyone",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Artificial Intelligence",
+    "rating": 4.7,
+    "time": "6 hours"
+},
+{
+    "name": "Fundamentals of Web Development",
+    "description": "Learn the core principles of web development, including HTML, CSS, and JavaScript.",
+    "link": "https://www.udemy.com/course/fundamentals-of-web-development/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Web Development",
+    "rating": 4.5,
+    "time": "20 hours"
+},
+{
+    "name": "Advanced Data Structures and Algorithms",
+    "description": "An advanced course on data structures and algorithms, building on fundamental concepts.",
+    "link": "https://www.coursera.org/learn/advanced-data-structures",
+    "image": "https://about.coursera.org/static/img/favicon/favicon.png",
+    "type": "Data Structures & Algorithms",
+    "rating": 4.8,
+    "time": "14 hours"
+},
+{
+    "name": "Introduction to Web Development",
+    "description": "Get started with web development, including HTML, CSS, and JavaScript basics.",
+    "link": "https://www.freecodecamp.org/learn/",
+    "image": "https://www.freecodecamp.org/favicon.ico",
+    "type": "Web Development",
+    "rating": 4.8,
+    "time": "24 hours"
+},
+{
+    "name": "Data Science Essentials",
+    "description": "Learn the essential skills needed for data science, including data manipulation and analysis.",
+    "link": "https://www.edx.org/course/data-science-essentials",
+    "image": "https://www.edx.org/sites/default/files/favicon-16x16.png",
+    "type": "Data Science",
+    "rating": 4.7,
+    "time": "8 weeks"
+},
+{
+    "name": "AI Programming with Python",
+    "description": "A comprehensive course on AI programming with Python, including practical projects.",
+    "link": "https://www.udacity.com/course/ai-programming-python-nanodegree--nd089",
+    "image": "https://www.udacity.com/favicon.ico",
+    "type": "Artificial Intelligence",
+    "rating": 4.8,
+    "time": "3 months"
+},
+{
+    "name": "Introduction to HTML and CSS",
+    "description": "Learn the basics of HTML and CSS for web development.",
+    "link": "https://www.udemy.com/course/introduction-to-html-and-css/",
+    "image": "https://www.udemy.com/staticx/udemy/images/v7/favicon-120x120.png",
+    "type": "Web Development",
+    "rating": 4.6,
+    "time": "12 hours"
+}
+];
+
+
+
+export default { freeCSCourses };
